21323089679c086e54cd16b1d79c68437488d46a
Nathnael Alemu <nathnael.alemu@yahoo.com>
2017-08-17 15:52:06 -0500

    set boilerplate for Git facade

--- src/View/GitLet.java
+++ src/View/GitLet.java
@@ -4,4 +4,21 @@ package View;
  * Created by Nathnael on 8/17/2017.
  */
 public interface GitLet {
+    Void init();
+    Void add(String filename);
+    Void commit(String message);
+    Void rm(String filename);
+    Void log();
+    Void globalLog();
+    Void find(String message);
+    Void status();
+    Void checkout(String filename);
+    Void checkout(int commitId, String filename);
+    Void checkoutV2(String brachname);
+    Void branch(String branchName);
+    Void rmBranch(String branchName);
+    Void reset(int commitId);
+    Void merge(String brachName);
+    Void rebase(String brachName);
+    Void interactiveRebase(String brachName);
 }
--- src/View/GitLetFacade.java
+++ src/View/GitLetFacade.java
@@ -4,4 +4,88 @@ package View;
  * Created by Nathnael on 8/17/2017.
  */
 public class GitLetFacade implements GitLet {
+    @Override
+    public Void init() {
+        return null;
+    }
+
+    @Override
+    public Void add(String filename) {
+        return null;
+    }
+
+    @Override
+    public Void commit(String message) {
+        return null;
+    }
+
+    @Override
+    public Void rm(String filename) {
+        return null;
+    }
+
+    @Override
+    public Void log() {
+        return null;
+    }
+
+    @Override
+    public Void globalLog() {
+        return null;
+    }
+
+    @Override
+    public Void find(String message) {
+        return null;
+    }
+
+    @Override
+    public Void status() {
+        return null;
+    }
+
+    @Override
+    public Void checkout(String filename) {
+        return null;
+    }
+
+    @Override
+    public Void checkout(int commitId, String filename) {
+        return null;
+    }
+
+    @Override
+    public Void checkoutV2(String brachname) {
+        return null;
+    }
+
+    @Override
+    public Void branch(String branchName) {
+        return null;
+    }
+
+    @Override
+    public Void rmBranch(String branchName) {
+        return null;
+    }
+
+    @Override
+    public Void reset(int commitId) {
+        return null;
+    }
+
+    @Override
+    public Void merge(String brachName) {
+        return null;
+    }
+
+    @Override
+    public Void rebase(String brachName) {
+        return null;
+    }
+
+    @Override
+    public Void interactiveRebase(String brachName) {
+        return null;
+    }
 }
