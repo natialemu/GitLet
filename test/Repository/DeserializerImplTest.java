f74ae338cbc476b4cffa76a24e0682aadad80f71
Nathnael Alemu <nathnael.alemu@yahoo.com>
2017-10-08 18:09:31 -0500

    added resource files

--- test/Repository/DeserializerImplTest.java
+++ test/Repository/DeserializerImplTest.java
@@ -4,10 +4,12 @@ import Model.CommitTree;
 import Model.Snapshot;
 import Model.State.GitVCS;
 import Model.Tools.RandomFileGenerator;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -39,7 +41,7 @@ class DeserializerImplTest {
         int commitIDofFirstTestFile = 0;
         String serializedTextFile = serializer.serializeTextFile(firstTestFile,commitIDofFirstTestFile);
         File deserializedFile = deserializer.deserializeTextFile(serializedTextFile);
-        assert (originalFile.equals(deserializedFile));
+
 
         String secondFile = RandomFileGenerator.createRandomText();
 
@@ -47,7 +49,13 @@ class DeserializerImplTest {
         int commitIDofSecondFile = 1;
         String serializedTextFile2 = serializer.serializeTextFile(secondFile,commitIDofSecondFile);
         File deserializedFile2 = deserializer.deserializeTextFile(serializedTextFile2);
-        assert (originalSecondFile.equals(deserializedFile2));
+
+        try {
+            assert (FileUtils.contentEquals(originalSecondFile,deserializedFile2));
+            assert (FileUtils.contentEquals(originalFile, deserializedFile));
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
 
     }
 
